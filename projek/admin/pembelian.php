997fd5659132f3f0cef2bda984ec18a4d01e9f40
lihaong <54676003+lihaong@users.noreply.github.com>
2021-11-24 23:04:29 +0700

    first commit

---
+++ projek/admin/pembelian.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+error_reporting(0);
+include '/Applications/XAMPP/xamppfiles/htdocs/projek/functions.php';
+include '../asset.php';
+$data = query("SELECT * FROM pembelian");
+$tabel = 'pembelian';
+$col = 'Id_pembelian';
+
+if (isset($_POST["delete"])) {
+    if (delete($_POST, $tabel, $col) > 0) {
+        echo " <script> alert('Data berhasil dihapus!'); </script> ";
+        header("Location: pembelian.php");
+    } else {
+        echo " <script> alert('Data tidak berhasil dihapus!'); </script> ";
+    }
+}
+
+if (isset($_POST["update"])) {
+    $temp = $_POST['id'];
+    header("Location: updateUser.php?id=$temp");
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/logo.png" />
+    <title>
+        Purchase Data
+    </title>
+    <link rel="normal" href="../Normalize/Normalize.css" />
+    <link rel="stylesheet" href="../style.css" />
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <div class="flex">
+                <div class="img-wrapper">
+                    <a href="index.php">
+                        <img style="max-width: 200px" src="../images/rog-logo@3x.png" alt="logo" />
+                    </a>
+                </div>
+
+                <ul id="menu" class="menu">
+                    <li>
+                        <a class="btn" href="../admin/">User</a>
+                    </li>
+                    <li>
+                        <a class="btn" href="../admin/product.php">Product</a>
+                    </li>
+                    <li>
+                        <a class="btn" href="../admin/pembelian.php">Purchase</a>
+                    </li>
+                    <li>
+                        <a class="btn login" href="../<?php echo $tempLoc; ?>"><?php echo $var; ?></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="container">
+        <h2 class="line-top">User Data</h2>
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>Checkout Code</th>
+                    <th>User ID</th>
+                    <th>Total Price</th>
+                    <th>Total Items</th>
+                    <th>Items</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <?php foreach ($data as $row) : ?>
+                <tbody>
+                    <tr>
+                        <td><?= $row[$col]; ?></td>
+                        <td><?= $row["Id_Pelanggan"]; ?></td>
+                        <td><?= $row["Tot_Harga"]; ?></td>
+                        <td><?= $row["Tot_Barang"]; ?></td>
+                        <td><?= $row["DaftarBarang"]; ?></td>
+                        <td>
+                            <form method="POST">
+                                <button id="button" style="text-decoration: none; background:transparent; margin: 4px;" type="submit" name="delete">
+                                    <img src="../images/delete-icon.svg" alt="delete" />
+                                </button>
+                                <input type="hidden" name="id" value="<?= $row[$col]; ?>">
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        </table>
+    </section>
+</body>
+
+</html>
