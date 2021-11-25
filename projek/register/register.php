19ad069374c07693c754bc15d9b6ce2fc8acb909
lihaong <54676003+lihaong@users.noreply.github.com>
2021-11-25 13:19:48 +0700

    Update register.php

--- projek/register/register.php
+++ projek/register/register.php
@@ -1,5 +1,6 @@
 <?php
-include '/Applications/XAMPP/xamppfiles/htdocs/projek/functions.php';
+include '../functions.php';
+
 if (isset($_POST["register"])) {
   if (registrasi($_POST) > 0) {
     echo '<script>alert("Berhasil menambahkan User")</script>';
