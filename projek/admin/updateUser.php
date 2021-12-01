5b35aa9429171a750db7005e8eaa6664a158aadb
lihaong <54676003+lihaong@users.noreply.github.com>
2021-12-01 10:25:51 +0700

    update User update

--- projek/admin/updateUser.php
+++ projek/admin/updateUser.php
@@ -3,11 +3,22 @@
 session_start();
 error_reporting(0);
 
-include '..functions.php';
+include '../functions.php';
 
 if ($_GET['id'] > 0) {
     $tempUser = $_GET['id'];
 }
+$data = query("SELECT * FROM pelanggan where Id_Pelanggan = '$tempUser'");
+foreach ($data as $row) :
+    $tempNama = $row['Nama'];
+    $tempEmail = $row['Email'];
+    $tempStreet = $row['Street'];
+    $tempCountry = $row['Country'];
+    $tempCity = $row['City'];
+    $tempDistrict = $row['District'];
+    $tempZip = $row['zip'];
+    $tempPhone = $row['Phone'];
+endforeach;
 if (isset($_POST["Update"])) {
     if (updateUser($_POST, $tempUser) > 0) {
         echo " <script> alert('Data berhasil diupdate!'); </script> ";
@@ -16,6 +27,7 @@ if (isset($_POST["Update"])) {
         echo " <script> alert('Data tidak berhasil diupdate!'); </script> ";
     }
 }
+
 
 ?>
 
@@ -46,13 +58,13 @@ if (isset($_POST["Update"])) {
                     <div class="separate">
                         <label class="label" for="txt-nama">Nama Lengkap</label>
                         <!-- name : nama -->
-                        <input class="input" type="text" id="txt-nama" name="nama" placeholder="Jhon Wick" required />
+                        <input class="input" type="text" id="txt-nama" name="nama" placeholder="<?php echo $tempNama ?>" required />
                     </div>
 
                     <div class="separate">
                         <label class="label" for="txt-email">Email Address</label>
                         <!-- name : email -->
-                        <input class="input" type="email" id="txt-email" name="email" placeholder="johndoe@gmail.com" required />
+                        <input class="input" type="email" id="txt-email" name="email" placeholder="<?php echo $tempEmail ?>" required />
                     </div>
                 </div>
 
@@ -84,7 +96,7 @@ if (isset($_POST["Update"])) {
                     <div class="separate">
                         <label class="label" for="txt-phone">Phone Number</label>
                         <!-- name : phone -->
-                        <input class="input" type="text" pattern="[0-9].{10,}" id="txt-phone" name="phone" placeholder="085643331986" required />
+                        <input class="input" type="text" pattern="[0-9].{10,}" id="txt-phone" name="phone" placeholder="<?php echo $tempPhone ?>" required />
                     </div>
                 </div>
 
@@ -95,28 +107,28 @@ if (isset($_POST["Update"])) {
                 <div class="field-group flex">
                     <div class="separate">
                         <!-- name : street -->
-                        <input type="street" name="street" class="input" placeholder="Street" required />
+                        <input type="street" name="street" class="input" placeholder="<?php echo $tempStreet ?>" required />
                     </div>
 
                     <div class="separate">
                         <!-- name : country -->
-                        <input type="country" name="country" class="input" placeholder="Country" required />
+                        <input type="country" name="country" class="input" placeholder="<?php echo $tempCountry ?>" required />
                     </div>
                 </div>
 
                 <div class="field-group flex">
                     <div class="separate">
                         <!-- name : city -->
-                        <input type="city" name="city" class="input" placeholder="City" required />
+                        <input type="city" name="city" class="input" placeholder="<?php echo $tempCity ?>" required />
                     </div>
 
                     <div class="separate">
                         <!-- name : districts -->
-                        <input type="districts" name="districts" class="input" placeholder="Districts" required />
+                        <input type="districts" name="districts" class="input" placeholder="<?php echo $tempDistrict ?>" required />
                     </div>
                     <div class="separate">
                         <!-- name : zip -->
-                        <input type="zip" name="zip" class="input" id="inputZip" placeholder="Zip" required />
+                        <input type="zip" name="zip" class="input" id="inputZip" placeholder="<?php echo $tempZip ?>" required />
                     </div>
                 </div>
                 <div class="field-group">
